6297c153257c71c4e834c59fd5ebc010797ae0ac
Artak Abrahamyan <abrahamyan@outlook.com>
2021-03-14 23:41:00 +0400

    New Commit

--- AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -8,10 +8,8 @@ namespace AbstractFactoryPattern
         static void Main(string[] args)
         {
             ShapeFactory shapefactory = new ShapeFactory();
-            Circle circle = (Circle) shapefactory.GetData(1).circle;
-            var mm = shape.DataItem;
-
-            Console.WriteLine(mm);
+            Circle circle = shapefactory.GetData((int)ShapeTypes.Circle).DataItem as Circle;
+            circle.GetInfo();
 
         }
     }
